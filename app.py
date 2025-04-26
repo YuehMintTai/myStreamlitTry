0444752e45216f80aa612740346c4e7859cde43a
SamyTai <ytai1123@gmail.com>
2025-04-26 17:50:54 +0800

    Add .streamlit and pages folders

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from faker import Faker
 fake=Faker('zh-TW')
 st.title(fake.sentence(20))
+st.header(fake.sentence(5))
+st.text(fake.sentence(50))
 st.write(fake.sentences(10))
 st.divider()
 st.text(fake.name())
